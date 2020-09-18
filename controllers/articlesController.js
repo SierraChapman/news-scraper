7cf0fbbe969d650774afc48c0e014bc6ff4fe8cf
Sierra Chapman <siechap@gmail.com>
2020-09-18 16:23:58 -0700

    set up function to scrape Washington Post

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -1,7 +1,21 @@
 const db = require("../models");
 
+// helper functions
+function scrapeWashingtonPost() {
+  const homepage = "https://www.washingtonpost.com/";
+  const articles = [];
+
+  console.log(`Scraping articles from ${homepage}...`);
+
+  return articles;
+}
+
+// controller
 module.exports = {
   findAll: function(req, res) {
+    const washPostArticles = scrapeWashingtonPost();
+    console.log(washPostArticles);
+
     db.Article.find({})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(500).json(err));
