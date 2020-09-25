6a5ef4901f7f62b4ccca394edb0794e20a2ca6dd
Sierra Chapman <siechap@gmail.com>
2020-09-24 17:00:23 -0700

    save new articles to database

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -7,41 +7,59 @@ function scrapeWashingtonPost() {
   const homepage = "https://www.washingtonpost.com/";
   console.log(`Scraping articles from ${homepage}...`);
 
-  return axios.get(homepage)
-    .then(response => {
-      const articles = [];
-      const $ = cheerio.load(response.data);
-      const $headlines = $(".font--headline");
+  return axios.get(homepage).then(response => {
+    const articles = [];
+    const $ = cheerio.load(response.data);
+    const $headlines = $(".font--headline");
 
-      for (let i = 0; i < $headlines.length; i++) {
-        const $headline = $headlines.eq(i);
+    for (let i = 0; i < $headlines.length; i++) {
+      const $headline = $headlines.eq(i);
 
-        const headline = $headline.text();
-        const byline = $headline.parent().siblings(".byline").text();
-        const url = $headline.find("a").attr("href");
-        const summary = $headline.parent().siblings(".bb").text();
-        
-        // articles must have headline and url at a minimum
-        if (headline && url) {
-          articles.push({ headline, byline, url, summary });
-        }
+      const headline = $headline.text();
+      const byline = $headline.parent().siblings(".byline").text();
+      const url = $headline.find("a").attr("href");
+      const summary = $headline.parent().siblings(".bb").text();
+
+      // articles must have headline and url at a minimum
+      if (headline && url) {
+        articles.push({ headline, byline, url, summary });
       }
+    }
 
-      return articles;
-    })
-    .catch(err => console.log(err));
+    console.log(`Collected data on ${articles.length} articles.`);
+    return articles;
+  }).catch(err => console.log(err));
 }
 
 // controller
 module.exports = {
-  findAll: function(req, res) {
-    scrapeWashingtonPost()
-      .then(articles => {
-        console.log(articles);
+  findAll: function (req, res) {
+    const newArticlesPromise = scrapeWashingtonPost();
+    const oldArticlesPromise = db.Article.find({}).then(articleData => {
+      // make an object containing each url in the database
+      const articleUrls = {};
+      articleData.forEach(article => {
+        articleUrls[article.url] = true;
+      });
 
-        return db.Article.find({});
-      })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(500).json(err));
+      return articleUrls;
+    });
+
+    // proceed after old and new articles have been retrieved
+    Promise.all([newArticlesPromise, oldArticlesPromise]).then(([newArticles, articleUrls]) => {
+      // filter out articles that are already saved
+      let articlesToSave = newArticles.filter(article => !articleUrls[article.url]);
+      // add missing fields
+      articlesToSave = articlesToSave.map(article => ({ ...article, date: Date.now(), comments: [] }));
+      // save
+      return db.Article.insertMany(articlesToSave);
+    }).then(savedArticles => {
+      console.log(`Saved ${savedArticles.length} new articles.`);
+      return db.Article.find({});
+    }).then(dbModel => {
+      return res.json(dbModel);
+    }).catch(err => {
+      return res.status(500).json(err)
+    });
   },
 };
